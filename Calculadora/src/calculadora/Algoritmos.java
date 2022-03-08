c4b79332ed32ebbedeb594a787a7bf27bfc6729d
AI-2003 <carlos.ibarraran@outlook.com>
2022-03-07 22:19:25 -0600

    AHHHH

--- Calculadora/src/calculadora/Algoritmos.java
+++ Calculadora/src/calculadora/Algoritmos.java
@@ -45,29 +45,6 @@ public class Algoritmos {
     }
     
     /**
-     * Hace los cálculos para que funcione las exponenciales en la computadora.
-     * @param num Número base.
-     * @param pow Potencia.
-     * @return Resultado de elevar num a pow.
-     */
-    private static double exp(double num, int pow){
-        double res;
-        
-        if(num==0)
-            res=0;
-        else
-            if(pow<0)
-                res=1/exp(num,-pow);
-            else{
-                res=1;
-                for(int i=1; i<=pow; i++)
-                    res*=num;
-            }
-        return res;
-    }
-    
-    
-    /**
      * Convierte pila de infija a postfija.
      * @param infija Pila sin procesar.
      * @return PilaA postfija .
@@ -146,6 +123,27 @@ public class Algoritmos {
         return infija;
     }
     
+    /**
+     * Hace los cálculos para que funcione las exponenciales en la computadora.
+     * @param num Número base.
+     * @param pow Potencia.
+     * @return Resultado de elevar num a pow.
+     */
+    private static double exp(double num, int pow){
+        double res;
+        
+        if(num==0)
+            res=0;
+        else
+            if(pow<0)
+                res=1/exp(num,-pow);
+            else{
+                res=1;
+                for(int i=1; i<=pow; i++)
+                    res*=num;
+            }
+        return res;
+    }
     
     
     /**
@@ -252,7 +250,7 @@ public class Algoritmos {
       * @param revisa Cadena a revisar.
       * @return boolean que indica si hay signos escritos de manera incorrecta.
       */
-     public static boolean revisaSigno(String revisa){
+    public static boolean revisaSigno(String revisa){
         boolean resp=true;
         int i=0;
         
@@ -277,8 +275,67 @@ public class Algoritmos {
       * @param revisa Cadena a revisar.
       * @return boolean que indica si hay signos, puntos o paréntesis escritos de manera incorrecta o algún caracter no reconocido.
       */
+    //public static boolean revisaSintaxis(String revisa){
+      // return revisaSigno(revisa) && analizaParentesis(revisa) && revisaPunto(revisa);
+    //}
+    
     public static boolean revisaSintaxis(String revisa){
-       return revisaSigno(revisa) && analizaParentesis(revisa) && revisaPunto(revisa);
+        boolean resp;
+        char next;
+        int i, j;
+        PilaA parentesis;
+        
+        resp=true;
+        i=0;
+        parentesis=new PilaA();
+        while(i<revisa.length()-1 && resp){
+            next=revisa.charAt(i+1);
+            switch(revisa.charAt(i)){
+                case '+', '*', '/', '^':
+                    if(next=='+'||next=='*'||next=='/'||next=='^'||next=='.'||next==')'){
+                        resp=false;
+                    }
+                    break;
+                case '-':
+                    if(next=='+'||next=='-'||next=='*'||next=='/'||next=='^'||next=='.'||next==')'){
+                        resp=false;
+                    }
+                    break;
+                case '.':
+                    if(next=='+'||next=='-'||next=='*'||next=='/'||next=='^'||next=='.'||next=='('||next==')')
+                        resp=false;
+                    else{
+                        j=i+1;
+                        while(j<revisa.length() && revisa.charAt(j)!='.' && revisa.charAt(j)>47 && revisa.charAt(j)<58)
+                            j++;
+                        if(j<revisa.length()&&revisa.charAt(j)=='.')
+                            resp=false;
+                    }
+                    break;
+                case '(':
+                    parentesis.push('(');
+                    if(next=='+'||next=='*'||next=='/'||next=='^'||next=='.'||next==')')
+                        resp=false;
+                    break;
+                case ')':
+                    if(next=='.'||next=='('||(next>47&&next<58)||parentesis.isEmpty())
+                        resp=false;
+                    else
+                        parentesis.pop();
+                    break;
+                default:
+                    if(revisa.charAt(i)<48||revisa.charAt(i)>57)
+                           resp=false;
+                    break;
+            }
+            i++;      
+        }
+        if(resp){
+            next=revisa.charAt(revisa.length()-1);
+            if(next=='+'||next=='-'||next=='*'||next=='/'||next=='^'||next=='.'||next=='(')
+                resp=false;
+        }
+        return resp;   
     }
     
     /**
@@ -287,7 +344,10 @@ public class Algoritmos {
       * @return String que es el nuevo String que se verá en la pantalla.
       */
     public static String borrarUltimo(String num){
-        return num.substring(0,num.length()-1);
+        if (!num.isEmpty())
+            num = num.substring(0,num.length()-1);
+        
+        return num;
     }
     
     /**
